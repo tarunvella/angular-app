e51edaf56bb396e9cf5f1af2f0fdbc1f3cef8118
Vella <tarun-kumar.vella@capgemini.com>
2018-10-04 14:07:17 +0530

    Bulding a Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // inline template
+  /*template: `
+  <div><h1>{{pageTitle}}</h1>
+  <div>My First Root Component</div>
+  </div>
+  `*/
+  template: `
+  <div><h1 innerText ={{pageTitle}}></h1>
+  <pm-products></pm-products>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Store Product Management';
+
+  
+
 }
